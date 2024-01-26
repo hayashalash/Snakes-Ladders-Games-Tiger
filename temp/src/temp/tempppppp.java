750a94602592c608251583d810447d3a885901f4
hayashalash <hayashalash7@gmail.com>
2024-01-26 23:26:21 +0200

    my first try

---
+++ temp/src/temp/tempppppp.java
@@ -0,0 +1,9 @@
+package temp;
+
+public class tempppppp {
+	public static void main ( String[] args) {
+		System.out.println("Hello World, it's my first time using Github wohoooooooooo!");
+		System.out.println("Bissssss asm3eniiiiii");
+	}
+	
+}
