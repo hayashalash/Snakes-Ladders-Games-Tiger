0e2ee0d642a3ce07c8230295a74e8f5d2c45f5d0
Haya Shalash <156433546+hayashalash@users.noreply.github.com>
2024-01-27 00:27:15 +0200

    Delete temp/src/temp directory

--- temp/src/temp/tempppppp.java
+++
@@ -1,9 +0,0 @@
-package temp;
-
-public class tempppppp {
-	public static void main ( String[] args) {
-		System.out.println("Hello World, it's my first time using Github wohoooooooooo!");
-		System.out.println("Bissssss asm3eniiiiii");
-	}
-	
-}
