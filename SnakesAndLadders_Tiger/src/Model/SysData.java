b1361a269e6d636f66f790cfd229d47ea42ee172
bisankadry <beeso1999@gmail.com>
2024-01-27 12:14:10 +0200

    SysData Update

---
+++ SnakesAndLadders_Tiger/src/Model/SysData.java
@@ -0,0 +1,9 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class SysData {
+	private ArrayList<Integer> EasyQuestions; // change "Integer" to class "Question" once created
+	private ArrayList<Integer> MediumQuestions; // change "Integer" to class "Question" once created
+	private ArrayList<Integer> HardQuestions; // change "Integer" to class "Question" once created
+}
