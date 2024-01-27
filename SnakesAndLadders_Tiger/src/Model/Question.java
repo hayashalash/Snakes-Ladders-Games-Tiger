2e57f268cd4232163b70c10f2fcc731d30144ce0
hayashalash <hayashalash7@gmail.com>
2024-01-27 17:32:30 +0200

    Ques

--- SnakesAndLadders_Tiger/src/Model/Question.java
+++ SnakesAndLadders_Tiger/src/Model/Question.java
@@ -1,5 +1,15 @@
 package Model;
 
-public class Question {
+import java.util.ArrayList;
 
+public class Question {
+	
+	private ArrayList<Answer> answers;
+	private String question;
+	private String level;
+	private int correctAn;
+
+	private int stepsForward = 0;
+	private int stepsBack = 0;
+	
 }
