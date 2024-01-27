9a0ff867f7c5f4b0c5b2e252fd28ffc1ffecafae
hayashalash <hayashalash7@gmail.com>
2024-01-27 22:21:59 +0200

    updating answers

--- SnakesAndLadders_Tiger/src/Model/Answer.java
+++ SnakesAndLadders_Tiger/src/Model/Answer.java
@@ -1,7 +1,10 @@
 package Model;
 
+import java.util.Objects;
+
 public class Answer {
 
+	private static int idCounter = 1;
 	private int answerID;
 	private String answer;
 	// private boolean isCorrect;
@@ -17,6 +20,35 @@ public class Answer {
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
-
+	@Override
+	public int hashCode() {
+		return Objects.hash(answer, answerID);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		return Objects.equals(answer, other.answer) && answerID == other.answerID;
+	}
+	public Answer(int answerID, String answer) {
+		super();
+		answerID = idCounter++;
+		this.answerID = answerID;
+		this.answer = answer;
+	}
+	public Answer(String answer) {
+		super();
+		this.answer = answer;
+	}
+	@Override
+	public String toString() {
+		return "Answer [answer=" + answer + "]";
+	}
+	
 	
 }
