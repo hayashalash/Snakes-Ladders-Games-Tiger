5f71b725fe4c8bb516aeb2aef0b66b180f23488d
areen100 <areenyassin00@gmail.com>
2024-01-28 21:12:59 +0200

    classes

--- SnakesAndLadders_Tiger/src/Model/Dice.java
+++ SnakesAndLadders_Tiger/src/Model/Dice.java
@@ -66,5 +66,7 @@ public class Dice {
 	   return  playingOrder;      
 
 	}
+	
+	
 
 }
--- SnakesAndLadders_Tiger/src/Model/Player.java
+++ SnakesAndLadders_Tiger/src/Model/Player.java
@@ -1,5 +1,66 @@
 package Model;
 
 public class Player {
+	private int playerId;
+	private String playerName;
+	private Color playerColor;
+	private int playerPlace=1;
+	private int hasStarted=0;
+	
+	public Player(int playerId, String playerName, Color playerColor) {
+		super();
+		this.playerId = playerId;
+		this.playerName = playerName;
+		this.playerColor = playerColor;
+	}
+	
+	public void printPlayerAddedSuccess() {
+        System.out.println("New player" + playerName + "added successfully!");
+    }
 
+	public int getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public Color getPlayerColor() {
+		return playerColor;
+	}
+
+	public void setPlayerColor(Color playerColor) {
+		this.playerColor = playerColor;
+	}
+
+	public int getPlayerPlace() {
+		return playerPlace;
+	}
+
+	public void setPlayerPlace(int playerPlace) {
+		this.playerPlace = playerPlace;
+	}
+
+	public int getHasStarted() {
+		return hasStarted;
+	}
+
+	public void setHasStarted(int hasStarted) {
+		this.hasStarted = hasStarted;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [playerId=" + playerId + ", playerName=" + playerName + ", playerColor=" + playerColor
+				+ ", playerPlace=" + playerPlace + ", hasStarted=" + hasStarted + "]";
+	}
 }
