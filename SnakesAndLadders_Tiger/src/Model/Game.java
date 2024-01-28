7b9f1f61fdfbc0801665b27ed79cf892e53fdb40
hayashalash <hayashalash7@gmail.com>
2024-01-28 10:00:26 +0200

    Update Game.java

--- SnakesAndLadders_Tiger/src/Model/Game.java
+++ SnakesAndLadders_Tiger/src/Model/Game.java
@@ -1,5 +1,16 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Game {
 
+	private int GameID;
+	private GameType type;
+	private Dice dice;
+	private int playersNum;
+	private int gameDuration;
+	private ArrayList<Player> players;
+	private String winner;
+	
+	
 }
